00b97389b97b47791237a7ce526197cbc024ac35
Parikshit Misra <perryizgr8@live.in>
2018-01-29 23:51:58 +0530

    heap handling in progress

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,16 +1,26 @@
+#include <stddef.h>
+#include <stdint.h>
 #include <kernel/uart.h>
+#include <kernel/mem.h>
+#include <kernel/atag.h>
+#include <common/stdio.h>
+#include <common/stdlib.h>
 
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
 {
+    char buf[256];
     (void) r0;
     (void) r1;
     (void) atags;
 
     uart_init();
-    uart_puts("Hello, kernel world!\r\n");
+    puts("Initializing memory module...\r\n");
+    mem_init((atag_t *)atags);
 
+    puts("Hello, kernel world!\r\n");
     while(1) {
-        uart_putc(uart_getc());
-        uart_putc('\n');
+        gets(buf, 256);
+        puts(buf);
+        putc('\n');
     }
 }
--- src/kernel/mem.c
+++ src/kernel/mem.c
@@ -4,8 +4,23 @@
 #include <stdint.h>
 #include <stddef.h>
 
-extern uint8_t __end;
+// Heap stuffs
+static void heap_init(uint32_t heap_start);
+/* Implement kmalloc as a linked list of allocated segments.
+   Segments should be 4 byte aligned.
+   Use best fit algorithm to find an allocation. */
+typedef struct heap_segment {
+    struct heap_segment *next;
+    struct heap_segment *prev;
+    uint32_t is_allocated;
+    uint32_t segment_size; // Including this header
+} heap_segment_t;
 
+static heap_segment_t *heap_segment_list_head;
+
+// End heap stuffs
+
+extern uint8_t __end;
 static uint32_t num_pages;
 
 DEFINE_LIST(page);
@@ -14,12 +29,8 @@ IMPLEMENT_LIST(page);
 static page_t *all_pages_array;
 page_list_t free_pages;
 
-/* Implement kmalloc as a linked list of allocated segments.
-   Segments should be 4 byte aligned.
-   Use best fit algorithm to find an allocation. */
-
 void mem_init(atag_t *atags) {
-    uint32_t mem_size, page_array_len, kernel_pages, i;
+    uint32_t mem_size, page_array_len, kernel_pages, page_array_end, i;
 
     // Get total num of pages
     mem_size = get_mem_size(atags);
@@ -41,11 +52,23 @@ void mem_init(atag_t *atags) {
         all_pages_array[i].flags.kernel_page = 1;
     }
 
+    // Reserve KERNEL_HEAP_SIZE for the kernel heap
+    for(; i < kernel_pages + (KERNEL_HEAP_SIZE / PAGE_SIZE); i++) {
+        all_pages_array[i].vaddr_mapped = i * PAGE_SIZE; // Identity map the kernel pages
+        all_pages_array[i].flags.allocated = 1;
+        all_pages_array[i].flags.kernel_heap_page = 1;
+    }
+
     // Map the rest of the pages as unallocated, and add them to the free list
     for(; i < num_pages; i++) {
         all_pages_array[i].flags.allocated = 0;
         append_page_list(&free_pages, &all_pages_array[i]);
     }
+
+    // Init the heap
+    // TODO: is this for the kernel?
+    page_array_end = (uint32_t)&__end + page_array_len;
+    heap_init(page_array_end);
 }
 
 void *alloc_page(void) {
@@ -64,7 +87,7 @@ void *alloc_page(void) {
     // Get the address the physical page metadata refers to
     page_mem = (void *)((page - all_pages_array) * PAGE_SIZE);
 
-    // Zero the page, so data doesn't leak between user and kernel
+    // Zero the page, so data doesn't leak between users and kernel
     bzero(page_mem, PAGE_SIZE);
 
     return page_mem;
@@ -79,4 +102,10 @@ void free_page(void *ptr) {
     // Mark the page as free
     page->flags.allocated = 0;
     append_page_list(&free_pages, page);
+}
+
+static void heap_init(uint32_t heap_start) {
+    heap_segment_list_head = (heap_segment_t *)heap_start;
+    bzero(heap_segment_list_head, sizeof(heap_segment_t));
+    heap_segment_list_head->segment_size = KERNEL_HEAP_SIZE;
 }
