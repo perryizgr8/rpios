8b18224c584b8bd1aebefc108211917c2fee5877
Parikshit Misra <perryizgr8@live.in>
2018-02-05 21:11:29 +0530

    uart conditional on rpi model

--- include/kernel/uart.h
+++ include/kernel/uart.h
@@ -4,43 +4,95 @@
 #ifndef UART_H
 #define UART_H
 
+#ifdef MODEL_1
+#define PERIPHERAL_BASE 0x20000000
+#else
+#define PERIPHERAL_BASE 0x3F000000
+#endif
+
+#define PERIPHERAL_LENGTH 0x01000000
+
+typedef union uart_flags {
+    struct
+    {
+        uint8_t clear_to_send : 1;
+        uint8_t data_set_ready : 1;
+        uint8_t data_carrier_detected : 1;
+        uint8_t busy : 1;
+        uint8_t receive_queue_empty : 1;
+        uint8_t transmit_queue_full : 1;
+        uint8_t receive_queue_full : 1;
+        uint8_t transmit_queue_empty : 1;
+        uint8_t ring_indicator : 1;
+        uint32_t padding : 23;
+    };
+    uint32_t as_int;
+} uart_flags_t;
+
+typedef union uart_control {
+    struct
+    {
+        uint8_t uart_enabled : 1;
+        uint8_t sir_enabled : 1;
+        uint8_t sir_low_power_mode : 1;
+        uint8_t reserved : 4;
+        uint8_t loopback_enabled : 1;
+        uint8_t transmit_enabled : 1;
+        uint8_t receive_enabled : 1;
+        uint8_t data_transmit_ready : 1;
+        uint8_t request_to_send : 1;
+        uint8_t out1 : 1;
+        uint8_t out2 : 1;
+        uint8_t rts_hardware_flow_control_enabled : 1;
+        uint8_t cts_hardware_flow_control_enabled : 1;
+        uint16_t padding;
+    };
+    uint32_t as_int;
+} uart_control_t;
+
 enum
 {
     // The GPIO registers base address.
-    GPIO_BASE = 0x3F200000, //for rpi2 & 3, 0x20200000 for rpi1
+    GPIO_BASE = PERIPHERAL_BASE + 0x200000,
+    // The offsets for each register.
 
+    // Controls actuation of pull up/down to ALL GPIO pins.
     GPPUD = (GPIO_BASE + 0x94),
+
+    // Controls actuation of pull up/down for specific GPIO pin.
     GPPUDCLK0 = (GPIO_BASE + 0x98),
 
     // The base address for UART.
-    UART0_BASE = 0x3F201000, //for rpi2 & 3, 0x20201000 for rpi1
+    UART0_BASE = PERIPHERAL_BASE + 0x201000,
 
-    UART0_DR     = (UART0_BASE + 0x00),
+    // The offsets for each register for the UART
+    UART0_DR = (UART0_BASE + 0x00),
     UART0_RSRECR = (UART0_BASE + 0x04),
-    UART0_FR     = (UART0_BASE + 0x18),
-    UART0_ILPR   = (UART0_BASE + 0x20),
-    UART0_IBRD   = (UART0_BASE + 0x24),
-    UART0_FBRD   = (UART0_BASE + 0x28),
-    UART0_LCRH   = (UART0_BASE + 0x2C),
-    UART0_CR     = (UART0_BASE + 0x30),
-    UART0_IFLS   = (UART0_BASE + 0x34),
-    UART0_IMSC   = (UART0_BASE + 0x38),
-    UART0_RIS    = (UART0_BASE + 0x3C),
-    UART0_MIS    = (UART0_BASE + 0x40),
-    UART0_ICR    = (UART0_BASE + 0x44),
-    UART0_DMACR  = (UART0_BASE + 0x48),
-    UART0_ITCR   = (UART0_BASE + 0x80),
-    UART0_ITIP   = (UART0_BASE + 0x84),
-    UART0_ITOP   = (UART0_BASE + 0x88),
-    UART0_TDR    = (UART0_BASE + 0x8C),
+    UART0_FR = (UART0_BASE + 0x18),
+    UART0_ILPR = (UART0_BASE + 0x20),
+    UART0_IBRD = (UART0_BASE + 0x24),
+    UART0_FBRD = (UART0_BASE + 0x28),
+    UART0_LCRH = (UART0_BASE + 0x2C),
+    UART0_CR = (UART0_BASE + 0x30),
+    UART0_IFLS = (UART0_BASE + 0x34),
+    UART0_IMSC = (UART0_BASE + 0x38),
+    UART0_RIS = (UART0_BASE + 0x3C),
+    UART0_MIS = (UART0_BASE + 0x40),
+    UART0_ICR = (UART0_BASE + 0x44),
+    UART0_DMACR = (UART0_BASE + 0x48),
+    UART0_ITCR = (UART0_BASE + 0x80),
+    UART0_ITIP = (UART0_BASE + 0x84),
+    UART0_ITOP = (UART0_BASE + 0x88),
+    UART0_TDR = (UART0_BASE + 0x8C),
 };
 
 static inline void mmio_write(uint32_t reg, uint32_t data);
 static inline uint32_t mmio_read(uint32_t reg);
 static inline void delay(int32_t count);
 void uart_init();
+uart_flags_t read_flags(void);
 void uart_putc(unsigned char c);
 unsigned char uart_getc();
-void uart_puts(const char* str);
+void uart_puts(const char *str);
 
 #endif /* UART_H */
